8c082f0bb38a256af88e45819eebef22a61c6b69
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-11-11 18:53:15 +0500

    "update"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ 
 import './App.css'
-
+import PasswordGenerator from './Components/Password-Generator'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
- <h1>hellow World</h1>
- <input type="range" />
+ <PasswordGenerator/>
     </>
   )
 }
---
+++ src/Components/Password-Generator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function PasswordGenerator() {
+  return (
+    <>
+    <div className='w-full h-screen text-white  bg-blue-400' >
+        
+        
+        
+         </div>
+  </>  
+  )
+}
