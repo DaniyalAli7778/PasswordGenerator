dbbcb2fd055039fe2733675d8583e165450a89d2
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-11-12 19:28:01 +0500

    "update"

--- src/Components/Password-Generator.jsx
+++ src/Components/Password-Generator.jsx
@@ -1,23 +1,52 @@
-import React from "react";
-
+ 
+ 
 export default function PasswordGenerator() {
+  // let [length, setLength] = useState(8);
+
+  // let[numberAllowed,setNumberAllowed]=useState(false);
+  // let[charAllowed,setCharAllowed]=useState(false);
+
+  // const lengthUpdate = () => {
+  //   let newLength = length++;
+  //   setLength(newLength);
+  // };
   return (
     <>
       <div className="w-full h-screen text-white m-0 p-0 flex justify-center items-center	">
         <div className="bg-gray-500 h-36 w-96 p-2 flex flex-col justify-center">
-         <span> <input type="text" className="w-46 bg-white text-orange-500 p-1 mb-4 " />
-          <button className="bg-blue-600 text-white p-1.5  rounded-lg  "> Copy</button>
+          <span>
+          
+            <input
+              type="text"
+              className="w-46 bg-white text-orange-500 p-1 mb-4 "
+              value={length}
+            />
+            <button className="bg-blue-600 text-white p-1.5  rounded-lg  ">
+            
+              Copy
+            </button>
           </span>
           <br />
           <span>
-          <input type="range" name="" id=""  className="w-15"/>
-          <label htmlFor="">Length ({length})</label> 
-          <input type="checkbox" name="" id=""className="ml-3" />
-          <label htmlFor="" >Numbers</label> 
-          <input type="checkbox" name="" id="" className="ml-3"/>
-          <label htmlFor="" >Letters</label> 
+            <input
+              type="range"
+              name=""
+              id=""
+            
+            />
+            <label htmlFor="" className="text-orange-500">
+              Length ({length})
+            </label>
+            <input type="checkbox" name="" id="" className="ml-2" />
+            <label htmlFor="" className="text-orange-500">
+              Numbers
+            </label>
+            <input type="checkbox" name="" id="" className="ml-2" />
+            <label htmlFor="" className="text-orange-500">
+              Letters
+            </label>
           </span>
-        </div> 
+        </div>
       </div>
     </>
   );
