d1a25b073ddc553baeedf5af9bfe124c83ca5781
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-11-13 10:06:21 +0500

    "add "

--- src/Components/Password-Generator.jsx
+++ src/Components/Password-Generator.jsx
@@ -1,47 +1,45 @@
- 
- 
+import { useCallback, useState } from "react";
+
 export default function PasswordGenerator() {
-  // let [length, setLength] = useState(8);
+  let [length, setLength] = useState(8);
 
-  // let[numberAllowed,setNumberAllowed]=useState(false);
-  // let[charAllowed,setCharAllowed]=useState(false);
+  let [numberAllowed, setNumberAllowed] = useState(false);
+  let [charAllowed, setCharAllowed] = useState(false);
+  let [password, setPassword] = useState("");
+  const passwordgenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += 123456789;
+    for (let i = 0; i <= array.length; i++) {
+      pass = Math.floor(Math.random(str));
+    }
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
-  // const lengthUpdate = () => {
-  //   let newLength = length++;
-  //   setLength(newLength);
-  // };
   return (
     <>
       <div className="w-full h-screen text-white m-0 p-0 flex justify-center items-center	">
         <div className="bg-gray-500 h-36 w-96 p-2 flex flex-col justify-center">
           <span>
-          
             <input
               type="text"
               className="w-46 bg-white text-orange-500 p-1 mb-4 "
-              value={length}
             />
             <button className="bg-blue-600 text-white p-1.5  rounded-lg  ">
-            
               Copy
             </button>
           </span>
           <br />
           <span>
-            <input
-              type="range"
-              name=""
-              id=""
-            
-            />
-            <label htmlFor="" className="text-orange-500">
+            <input type="range" name="" id="" className="" />
+
+            <label htmlFor="" className="text-orange-500 size-1">
               Length ({length})
             </label>
-            <input type="checkbox" name="" id="" className="ml-2" />
+            <input type="checkbox" name="" id="" className="ml-2 p-1" />
             <label htmlFor="" className="text-orange-500">
               Numbers
             </label>
-            <input type="checkbox" name="" id="" className="ml-2" />
+            <input type="checkbox" name="" id="" className="ml-2 p-1" />
             <label htmlFor="" className="text-orange-500">
               Letters
             </label>
