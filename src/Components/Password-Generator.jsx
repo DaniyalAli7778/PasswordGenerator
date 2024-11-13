8e382a2f66c83ea6fff03414b8344bd302fd6f5d
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-11-13 15:26:23 +0500

    "Project Ends here "

--- src/Components/Password-Generator.jsx
+++ src/Components/Password-Generator.jsx
@@ -3,13 +3,16 @@ import { useCallback, useState,useEffect,useRef} from "react";
 export default function PasswordGenerator() {
   let [length, setLength] = useState(8);
 
-  let [numberAllowed, setNumberAllowed] = useState(false);
-  let [charAllowed, setCharAllowed] = useState(false);
+  let [numberAllowed, setNumberAllowed] = useState(true);
+  let [charAllowed, setCharAllowed] = useState(true);
   let [password, setPassword] = useState("");
   const passwordGenerator = useCallback(() => {
     var pass =  "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) str += "123456789";
+    else{
+      str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    }
     if (charAllowed) str +=  "@#$%^&*()_+{}[]=--";
     for (let i = 1; i <=  length; i++) {
       let char= Math.floor(Math.random() * str.length + 1 );
@@ -31,7 +34,9 @@ useEffect(()=>{
 },[length, numberAllowed, charAllowed,passwordGenerator, ])
   return (
     <>
-      <div className="w-full h-screen text-white m-0 p-0 flex justify-center items-center	">
+      <div className="w-full h-screen text-white m-0 p-0 flex justify-center items-center relative	">
+
+        <h1 className="absolute top-8  text-6xl">Password Generator</h1>
         <div className="bg-gray-500 h-36 w-full p-2 flex flex-col justify-center items-center">
           <span>
             <input
@@ -49,7 +54,7 @@ useEffect(()=>{
               setText("copied !");
              
               setTimeout(() => {
-                alert("Text is Copied")
+                alert("Password  is Copied")
                 setText("copy")
               }, 1000);
              
